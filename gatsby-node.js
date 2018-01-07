9fd20edf2b230a25b7046e0be0d2223122808c3e
Allan Cassiano Weber <allanweber@outlook.com>
2018-01-07 16:10:32 -0200

    First commit

---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+    const { createPage } = boundActionCreators;
+
+    const postTemplate = path.resolve('src/templates/post.js');
+
+    return graphql(`{
+        allMarkdownRemark {
+            edges {
+                node {
+                    html
+                    id
+                    frontmatter{
+                        path
+                        title
+                        date
+                    }
+                }
+            }
+        }
+    }`)
+        .then(result => {
+            if (result.errors) {
+                return Promise.reject(result.errors);
+            }
+
+            result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+                createPage({
+                    path: node.frontmatter.path,
+                    component: postTemplate
+                })
+            })
+        })
+}
---
+++ src/pages/20180106/index.md
@@ -0,0 +1,7 @@
+---
+path: '/first-post'
+title: 'First Blog Post'
+date: "2017-07-12T17:12:33.962Z"
+---
+
+ Hello! this is my first post
